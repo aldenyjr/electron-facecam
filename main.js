69d86608b7a34c810545c5361d011216c24c4e56
Aldeny Junior <aldenyjr.dev@gmail.com>
2024-02-07 13:52:24 -0300

    Projeto Finalziado

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, screen } = require("electron");
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 640,
-    height: 360,
+    width: 405,
+    height: 405,
     webPreferences: {
       nodeIntegration: true,
     },
+    frame: false,
+    transparent: true,
+    resizable: false,
   });
 
   win.loadFile("index.html");
+  win.on("will-move", (event, rect) => {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    rect.width + rect.x + 2 > width && event.preventDefault();
+    rect.height + rect.y + 2 > height && event.preventDefault();
+    rect.x < 0 && event.preventDefault();
+    rect.y < 0 && event.preventDefault();
+  });
 };
 
 app.whenReady().then(() => {
